a9f3fff8b772286184760620b539d520d0da3eb0
kamillenikolajsen <knikolajsen4@gmail.com>
2021-11-23 16:27:56 +0100

    IntelliJ project created

---
+++ src/test/java/projectCalculationTool/ProjectCalculationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package projectCalculationTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCalculationToolApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
