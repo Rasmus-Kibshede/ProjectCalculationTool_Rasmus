35c477ad232423d3d832a5d12a21f619cb70e3b0
Michala Nybroe <michalanim@gmail.com>
2021-11-24 15:41:26 +0100

    add user ddd and html and css - stealing alpha solutions header and footer

--- src/main/java/projectCalculationTool/user/UserController.java
+++ src/main/java/projectCalculationTool/user/UserController.java
@@ -1,5 +1,16 @@
 package projectCalculationTool.user;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
+@Controller
+public class UserController {
+    UserService userService = new UserService(new UserRepository());
+
+    @PostMapping("/login")
+    public String login(WebRequest webRequest) {
+        webRequest.setAttribute("user", "hej", WebRequest.SCOPE_SESSION);
+        return "overview";
+    }
 }
--- src/main/java/projectCalculationTool/user/UserService.java
+++ src/main/java/projectCalculationTool/user/UserService.java
@@ -1,4 +1,9 @@
 package projectCalculationTool.user;
 
 public class UserService {
+    UserRepositoryInterface userRepositoryInterface;
+
+    public UserService(UserRepositoryInterface userRepositoryInterface) {
+        this.userRepositoryInterface = userRepositoryInterface;
+    }
 }
