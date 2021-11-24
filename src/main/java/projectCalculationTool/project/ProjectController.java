26675418bfe01a7943a0f9d1f7b00f9750ac703f
kamillenikolajsen <knikolajsen4@gmail.com>
2021-11-24 15:31:18 +0100

    Add content in project classes

--- src/main/java/projectCalculationTool/project/ProjectController.java
+++ src/main/java/projectCalculationTool/project/ProjectController.java
@@ -1,4 +1,37 @@
 package projectCalculationTool.project;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
+
+
+@Controller
+
 public class ProjectController {
+
+  private final ProjectService PROJECT_SERVICE = new ProjectService(new ProjectRepository());
+
+  @GetMapping("/overview")
+  public String projectPage(WebRequest webRequest, Model model){
+
+    int projectID = Integer.parseInt(webRequest.getParameter("id"));
+    Project project = PROJECT_SERVICE.readProject(projectID);
+    model.addAttribute("projectname", project);
+
+    return "overview";
+  }
+
+  @PostMapping("/addproject")
+  public String createProject(WebRequest webRequest){
+
+    String projectName = webRequest.getParameter("name");
+
+    Project project = new Project();
+    project.setName(projectName);
+    PROJECT_SERVICE.createProject(project);
+
+    return "redirect:/overview";
+  }
 }
--- src/main/java/projectCalculationTool/project/ProjectService.java
+++ src/main/java/projectCalculationTool/project/ProjectService.java
@@ -1,4 +1,17 @@
 package projectCalculationTool.project;
 
 public class ProjectService {
+
+  private ProjectRepository projectRepository;
+
+  public ProjectService(ProjectRepository projectRepository){
+    this.projectRepository = projectRepository;
+  }
+  public void createProject(Project project){
+    projectRepository.create(project);
+  }
+
+  public Project readProject(int projectID){
+    return projectRepository.read(projectID);
+  }
 }
