14a1e5ad96a0657867fc27c6ebb264a9e97be162
Rasmus Kibshede <2740ralle@gmail.com>
2021-11-25 10:23:56 +0100

    DB connection + junit test of DB connection

--- src/main/java/projectCalculationTool/util/DBManager.java
+++ src/main/java/projectCalculationTool/util/DBManager.java
@@ -8,25 +8,24 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DBManager {
-  private static String user;
-  private static String password;
-  private static String url;
-  private static Connection connection = null;
+    private static String user;
+    private static String password;
+    private static String url;
+    private static Connection connection = null;
 
-  public static Connection getConnection(){
-    if(connection != null){
-      return connection;
+    public static Connection getConnection() {
+        if (connection != null) return connection;
+
+        user = System.getenv("user");
+        password = System.getenv("password");
+        url = System.getenv("url");
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace(); // MAKE OWN EXCEPTION????
+        }
+
+        return connection;
     }
-    user = System.getenv("user");
-    password = System.getenv("password");
-    url = System.getenv("url");
-
-    try{
-      connection = DriverManager.getConnection(user, password, url);
-    }catch (SQLException e){
-      e.printStackTrace(); // MAKE OWN EXCEPTION????
-    }
-
-    return connection;
-  }
 }
