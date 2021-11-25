3000741e3d34d6672471249de0660e777dcfb7a4
August <Augusthauerslev@outlook.com>
2021-11-25 16:33:07 +0100

    created branch project fix

--- src/main/java/projectCalculationTool/project/ProjectController.java
+++ src/main/java/projectCalculationTool/project/ProjectController.java
@@ -6,25 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
-import java.util.Objects;
-
 @Controller
 public class ProjectController {
 
     private final ProjectService PROJECT_SERVICE = new ProjectService(new ProjectRepository());
 
-
     @GetMapping("/profile")
     public String projectPage(WebRequest webRequest, Model model) {
 
-        int projectID = Integer.parseInt(Objects.requireNonNull(webRequest.getParameter("id")));
+        int projectID = Integer.parseInt(webRequest.getParameter("id"));
 
         Project project = PROJECT_SERVICE.readProject(projectID);
 
-//Logik skal ikke ligge her skal ligge i repository
+        //Logik skal ikke ligge her skal ligge i repository
         if (project != null) {
             model.addAttribute("project", project);
-            return "/profile";
+            return "profile";
         }
 
         return "redirect:/";
