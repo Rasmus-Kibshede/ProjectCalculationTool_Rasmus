4a548c7fc79f15ba6df1567344dafe06e5cc23b2
August <Augusthauerslev@outlook.com>
2021-11-26 11:50:06 +0100

    created branch project fix

--- src/main/java/projectCalculationTool/project/ProjectController.java
+++ src/main/java/projectCalculationTool/project/ProjectController.java
@@ -5,30 +5,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
+import projectCalculationTool.user.User;
+import projectCalculationTool.user.UserService;
 
 @Controller
 public class ProjectController {
 
     private final ProjectService PROJECT_SERVICE = new ProjectService(new ProjectRepository());
+    //private final UserService USER_SERVICE = new UserService();
 
-    @GetMapping("/profile")
-    public String projectPage(WebRequest webRequest, Model model) {
+    /*   @GetMapping("/profile")
+       public String projectPage(WebRequest webRequest, Model model) {
 
-        //int projectID = Integer.parseInt(webRequest.getParameter("id"));
-        //Get session with userID
+           //int projectID = Integer.parseInt(webRequest.getParameter("id"));
+           //Get session with userID
 
+         //  Project project = PROJECT_SERVICE.readProject(projectID);
+           //User user = USER_SERVICE.readUser()
+           //webRequest.setAttribute("user", user , webRequest.SCOPE_SESSION);
+           Logik skal ikke ligge her skal ligge i repository
+           //if (project != null) {
+           //   model.addAttribute("project", project);
+           //   return "profile";
+           }
 
-        Project project = PROJECT_SERVICE.readProject(projectID);
-
-        //Logik skal ikke ligge her skal ligge i repository
-        if (project != null) {
-            model.addAttribute("project", project);
-            return "profile";
-        }
-
-        return "redirect:/";
-    }
-
+       // return "redirect:/";
+       }
+    */
     @PostMapping("/addproject")
     public String createProject(WebRequest webRequest) {
 
