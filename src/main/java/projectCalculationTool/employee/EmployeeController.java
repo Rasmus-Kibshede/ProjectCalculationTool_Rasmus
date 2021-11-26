fc0f305bbe81a01eaa810498b9247baa5ba947d6
August <Augusthauerslev@outlook.com>
2021-11-26 13:29:42 +0100

    employee login expanded

--- src/main/java/projectCalculationTool/employee/EmployeeController.java
+++ src/main/java/projectCalculationTool/employee/EmployeeController.java
@@ -12,10 +12,14 @@ public class EmployeeController {
     public String login(WebRequest webRequest) {
         String mail = webRequest.getParameter("mail");
         String password = webRequest.getParameter("password");
-        Employee employee = employeeService.readUser(mail,password);
+        Employee employee = employeeService.readUser(mail, password);
 
+        if (employee != null) {
+            webRequest.setAttribute("employee", employee, WebRequest.SCOPE_SESSION);
+            return "redirect:/profile";
+        }
         //Set session with user obj
+        return "redirect:/";
 
-        return "/profile";
     }
 }
