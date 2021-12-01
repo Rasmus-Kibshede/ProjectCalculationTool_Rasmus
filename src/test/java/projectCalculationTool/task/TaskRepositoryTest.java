addb043117b4d76c26724f6d01ace2478defaeba
Michala Nybroe <michalanim@gmail.com>
2021-12-01 23:11:33 +0100

    add selenium test on login

--- src/test/java/projectCalculationTool/task/TaskRepositoryTest.java
+++ src/test/java/projectCalculationTool/task/TaskRepositoryTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import projectCalculationTool.subproject.SubProject;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,8 +21,25 @@ class TaskRepositoryTest {
     }
 /*
     @Test
-    public void testCreateProject
+    public void testCreateTask() throws SQLException {
+        // Arrange
+        SubProject subProject = new SubProject("Fødselsdagssnacks");
+        ArrayList<Task> tasks = new ArrayList<>();
+        Task bakeTask = new Task(4, "Bake cake");
+        Task shopTask = new Task(1, "Shop for chips and dip");
+        tasks.add(bakeTask);
+        tasks.add(shopTask);
+        subProject.setTasks(tasks);
 
+        // Act
+        taskRepository.createTask(subProject);
+
+        // Assert
+        assertTrue(bakeTask.getTaskID() != 0);
+        assertTrue(shopTask.getTaskID() != 0);
+    }
+ */
+/*
     @ParameterizedTest
     @CsvSource(value = {}, delimiter = ':')
     public void testValidCreate() {
---
+++ src/test/java/projectCalculationTool/ui/LoginControllerTest.java
@@ -0,0 +1,68 @@
+package projectCalculationTool.ui;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+// Referance til https://github.com/Tine-m/2.semUITestAutomation/blob/main/SystemTestAutomation/src/test/java/GoogleDemoTest.java
+public class LoginControllerTest {
+
+    private static WebDriver selenium;
+
+    @BeforeAll
+    public static void setUp() {
+        // Load selenium driver
+        // download chromeDriver.exe fra http://chromedriver.storage.googleapis.com/index.html?path=96.0.4664.45/
+        // opret mappe kaldet webdriver på c drev
+        System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver_1.exe");
+        selenium = new ChromeDriver();
+    }
+
+    @Test
+    public void canLogin() throws InterruptedException {
+        // Launch
+        selenium.navigate().to("http://localhost:8080");
+        WebElement email = selenium.findElement(By.name("email"));
+        email.sendKeys("test@yes.com");
+        WebElement password = selenium.findElement(By.name("password"));
+        password.sendKeys("123");
+        // når du submitter et element i en form submittes det hele (ergo nednestående trykkes enter)
+        password.submit();
+
+        assertEquals("Profile", selenium.getTitle());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"invalid@yes.com:123", "test@yes.com:345", "test:123", ":4", "test:", ":"}, delimiter = ':')
+    public void cannotLogin(String inputEmail, String inputPassword) {
+        // Launch
+        selenium.navigate().to("http://localhost:8080");
+        WebElement email = selenium.findElement(By.name("email"));
+        if (inputEmail != null) {
+            email.sendKeys(inputEmail);
+        }
+        WebElement password = selenium.findElement(By.name("password"));
+        if (inputPassword != null) {
+            password.sendKeys(inputPassword);
+        }
+        // når du submitter et element i en form submittes det hele (ergo nednestående trykkes enter)
+        password.submit();
+
+        assertNotEquals("Profile", selenium.getTitle());
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        // close browser
+        selenium.quit();
+    }
+}
