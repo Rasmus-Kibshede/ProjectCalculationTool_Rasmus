d5aebc78c8ab499edb0b6274fdbd5bb094dea1e7
August <Augusthauerslev@outlook.com>
2021-12-07 12:28:16 +0100

    unit tests for exceptions

--- src/main/java/projectCalculationTool/task/TaskService.java
+++ src/main/java/projectCalculationTool/task/TaskService.java
@@ -42,7 +42,7 @@ public class TaskService {
         if (taskName != null && !taskName.isEmpty() && taskName.length() <= 45) {
             return taskName;
         } else {
-            throw new ValidateException("Task name can't be null or longer than 45 characters.");
+            throw new ValidateException("Task name cannot be null or longer than 45 characters.");
         }
     }
 
