c5c328653daabbcedf06d5aaad0b5eacbda4d7d6
August <Augusthauerslev@outlook.com>
2021-12-08 10:34:56 +0100

    null tests for task

--- src/test/java/projectCalculationTool/subproject/SubProjectTest.java
+++ src/test/java/projectCalculationTool/subproject/SubProjectTest.java
@@ -7,6 +7,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class SubProjectTest {
 
     @Test
-    void calculateWorkdays() {
+    void correctHoursToWorkDays() {
+        //8 hours = 1 workday
+        //9 hours = 2 workdays
+        //arrange
+        int time=9;
+
+        //act
+        double calcWorkdays = (double)time/8;
+        int workdays = (int) Math.ceil(calcWorkdays);
+
+        int expected = 9;//number is in workdays
+        //assert
+        assertEquals(expected, workdays);
     }
+
+    @Test
+    void zeroTimeToWorkdays(){
+        int time = 0;
+
+        //act
+        double calcWorkdays = (double)time/8;
+        int workdays = (int) Math.ceil(calcWorkdays);
+
+        int expected = 0;//number is in workdays
+        //assert
+        assertEquals(expected, workdays);
+    }
+
+
 }
