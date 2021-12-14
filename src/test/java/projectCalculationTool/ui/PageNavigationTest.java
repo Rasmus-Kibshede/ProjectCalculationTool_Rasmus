287aaf666e19fd114e60b069a290e6662eef8349
Rasmus Kibshede <2740ralle@gmail.com>
2021-12-14 11:06:48 +0100

    Selenium test

--- src/test/java/projectCalculationTool/ui/PageNavigationTest.java
+++ src/test/java/projectCalculationTool/ui/PageNavigationTest.java
@@ -12,56 +12,59 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import projectCalculationTool.project.ProjectRepository;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 // Referance til https://github.com/Tine-m/2.semUITestAutomation/blob/main/SystemTestAutomation/src/test/java/GoogleDemoTest.java
 public class PageNavigationTest {
 
-    private static WebDriver selenium;
+  private static WebDriver selenium;
 
-    //Det viker ikke med min path...
-    // Så måske vi lige skal afprøve det med en anden ellers er det måske bare helt forkert...
+  //Det viker ikke med min path...
+  // Så måske vi lige skal afprøve det med en anden ellers er det måske bare helt forkert...
 
-    @BeforeAll
-    public static void setUp() {
-        // Load selenium driver
-        // download chromeDriver.exe fra http://chromedriver.storage.googleapis.com/index.html?path=96.0.4664.45/
+  @BeforeAll
+  public static void setUp() {
+    // Load selenium driver
+    // download chromeDriver.exe fra http://chromedriver.storage.googleapis.com/index.html?path=96.0.4664.45/
 
-        System.setProperty("webdriver.chrome.driver", "src/test/java/webDriver/chromedriver.exe");
-        selenium = new ChromeDriver();
+    System.setProperty("webdriver.chrome.driver", "src/test/java/webDriver/chromedriver.exe");
+    selenium = new ChromeDriver();
 
-        selenium.navigate().to("http://localhost:8080");
-        WebElement email = selenium.findElement(By.name("email"));
-        email.sendKeys("test@yes.com");
-        WebElement password = selenium.findElement(By.name("password"));
-        password.sendKeys("123");
-        // når du submitter et element i en form submittes det hele (ergo nednestående trykkes enter)
-        password.submit();
-    }
+    selenium.navigate().to("http://localhost:8080");
+    WebElement email = selenium.findElement(By.name("email"));
+    email.sendKeys("test@yes.com");
+    WebElement password = selenium.findElement(By.name("password"));
+    password.sendKeys("123");
+    // når du submitter et element i en form submittes det hele (ergo nednestående trykkes enter)
+    password.submit();
+  }
 
-    @Test
-    public void testNavigationValidtProject() {
-        selenium.navigate().to("http://localhost:8080/profile");
-        WebElement projectName = selenium.findElement(By.name("projectname"));
-        projectName.sendKeys("SeleniumProjectTest");
-        // Få fat i id på projekt
-        projectName.submit();
+  @Test
+  public void testNavigationValidtProject() {
+    //selenium.navigate().to("http://localhost:8080/profile");
+    WebElement projectName = selenium.findElement(By.name("projectname"));
+    projectName.sendKeys("SeleniumProjectTest");
+    // Få fat i id på projekt
+    projectName.submit();
 
-        assertEquals("Profile", selenium.getTitle());
-    }
+    //assertEquals("Profile", selenium.getTitle());
+    assertEquals("SeleniumProjectTest", selenium.findElement(By.name("SeleniumProjectTest")).getText());
+  }
 
-    @ParameterizedTest
-    @CsvSource(value = {"Project name cannot be longer then 45 characters this is the test"})
-    public void testNavigationInvalidtProject(String inputProjectName) {
+  @ParameterizedTest
+  @CsvSource(value = {"Project name cannot be longer then 45 characters this is the test"})
+  public void testNavigationInvalidtProject(String inputProjectName) {
 
-        selenium.navigate().to("http://localhost:8080/profile");
-        WebElement projectName = selenium.findElement(By.name("projectname"));
-        projectName.sendKeys(inputProjectName);
+    //selenium.navigate().to("http://localhost:8080/profile");
+    WebElement projectName = selenium.findElement(By.name("projectname"));
+    projectName.sendKeys(inputProjectName);
 
-        projectName.submit();
+    projectName.submit();
 
-        assertEquals("Profile", selenium.getTitle());
-    }
+    assertEquals("Profile", selenium.getTitle());
+  }
 /*
     @Test
     public void testNavigationValidtSubProject() {
@@ -116,24 +119,19 @@ public class PageNavigationTest {
 
         assertEquals("Project", selenium.getTitle());
     }
-
  */
 
-    @AfterAll
-    public static void tearDown() {
-        // delete project
-       /* ProjectRepository projectRepository = new ProjectRepository();
-        projectRepository.deleteProject(id);
-        selenium.navigate().to("http://localhost:8080/profile");
-        WebElement project = selenium.findElement(By.className("projectbox"));
-        project.
-                project.findElement(By.className("deletebtn"));
-        project.click();
-        project.submit();
+  @AfterAll
+  public static void tearDown() {
 
-        */
-
-        // tear down browser
-        selenium.quit();
+    //selenium.navigate().to("http://localhost:8080/profile");
+    WebElement project = selenium.findElement(By.name("SeleniumProjectTestdelete"));
+    if (project.isDisplayed()){
+      project.click();
     }
+
+
+    // tear down browser
+    selenium.quit();
+  }
 }
